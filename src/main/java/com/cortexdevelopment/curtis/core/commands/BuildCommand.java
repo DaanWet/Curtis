8bebd28aa83bcc2c95849ba05ccc3c9149b594b3
dwettinc <daan.wettinck@ugent.be>
2020-06-16 22:37:38 +0200

    Updated build command so it shows latest version

--- src/main/java/com/cortexdevelopment/curtis/core/commands/BuildCommand.java
+++ src/main/java/com/cortexdevelopment/curtis/core/commands/BuildCommand.java
@@ -2,9 +2,19 @@ package com.cortexdevelopment.curtis.core.commands;
 
 import com.cortexdevelopment.curtis.core.Main;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.json.JSONObject;
+
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class BuildCommand extends Command {
 
+    private final String GET_URL = "https://api.bintray.com/packages/dv8fromtheworld/maven/JDA/";
 
     public BuildCommand(){
         name = "build";
@@ -16,20 +26,43 @@ public class BuildCommand extends Command {
         for (String s : args){
             System.out.println(s);
         }
-        if(args[0].equalsIgnoreCase("gradle")){
-            e.getChannel().sendMessage("Stop using gradle, use maven you prick").queue();
-        } else if(args[0].equalsIgnoreCase("maven")) {
+        try {
+            URL url = new URL(GET_URL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setUseCaches(true);
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK){
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                JSONObject jsonObject = new JSONObject(response.toString());
+                String version = jsonObject.getString("latest_version");
 
+                if(args[0].equalsIgnoreCase("gradle")){
+                    e.getChannel().sendMessage(String.format("```implementation 'net.dv8tion:JDA:%s'```", version)).queue();
+                } else if(args[0].equalsIgnoreCase("maven")) {
+                    e.getChannel().sendMessage(String.format("```xml\n<dependency>\n" +
+                            "  <groupId>net.dv8tion</groupId>\n" +
+                            "  <artifactId>JDA</artifactId>\n" +
+                            "  <version>%s</version>\n" +
+                            "  <type>pom</type>\n" +
+                            "</dependency>```", version)).queue();
+                }
+            }
+        } catch (IOException exc){
 
-
-
-            e.getChannel().sendMessage("```xml\n<dependency>\n" +
-                    "  <groupId>net.dv8tion</groupId>\n" +
-                    "  <artifactId>JDA</artifactId>\n" +
-                    "  <version>4.1.1_135</version>\n" +
-                    "  <type>pom</type>\n" +
-                    "</dependency>```").queue();
         }
+
+
+
+
+
+
     }
 
     @Override
